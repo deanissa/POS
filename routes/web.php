b0a341e49a224b6d0bdf50b82a1385f155159f4f
deanissa <deanissa604@gmail.com>
2025-03-01 20:52:27 +0700

    Tugas Praktikum + Menyambungkan ke Database

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
 use resources\views\helloblade;
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +21,14 @@ use resources\views\helloblade;
 //    return view('welcome');
 //});
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::prefix('product')->group(function () {
+    Route::get('/', [ProductController::class, 'index'])->name('product.index');
+    Route::get('/category/food-beverage', [ProductController::class, 'foodBeverage'])->name('product.food-beverage');
+    Route::get('/category/beauty-health', [ProductController::class, 'beautyHealth'])->name('product.beauty-health');
+    Route::get('/category/home-care', [ProductController::class, 'homeCare'])->name('product.home-care');
+    Route::get('/category/baby-kid', [ProductController::class, 'babyKid'])->name('product.baby-kid');
+});
+Route::get('/user/{id}/name/{name}', [UserController::class, 'show'])->name('user.show');
+
+Route::get('/sales', [SalesController::class, 'index'])->name('sales.index');
