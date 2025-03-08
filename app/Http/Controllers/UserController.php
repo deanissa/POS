270535c84fdd7762a9ee6a50bababb0ee31bdd9e
deanissa <deanissa604@gmail.com>
2025-03-08 20:31:03 +0700

    mengambil rekaman dengan kueri tertentu (menggunakan findor)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,16 +5,14 @@ use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
 
+
 class UserController extends Controller
 {
     public function index()
     {
 
         //coba akses model UserModel
-        $user = UserModel :: findOr(20,['username','nama'], function() {
-            abort(404);
-
-        });
+        $user = UserModel :: findOrFail(1);
         return view('user', ['data' => $user]);
     }
 }
