e2833659c19e7934888010fe3b81e45634274c10
deanissa <deanissa604@gmail.com>
2025-03-10 12:37:59 +0700

    isDirty dan isClean

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,15 +12,29 @@ class UserController extends Controller
     {
 
         //coba akses model UserModel
-        $user = UserModel::firstOrNew(
-            ['username' => 'manager33'], // Dicari dulu berdasarkan username
-     [
-         'nama' => 'Manager tiga tiga',
+        $user = UserModel::create(
+        ['username' => 'manager55', 
+         'nama' => 'Manager55',
          'password' => Hash::make('12345'), // Password tetap dimasukkan
          'level_id' => 2
         ]
         );
-        $user -> save();
-        return view('user', ['data' => $user]);
+         $user->username = 'manager55';
+
+         $user->isDirty();//true
+         $user->isDirty('username');//true
+         $user->isDirty('nama');//false
+         $user->isDirty('nama', 'username');//false
+
+         $user->isClean();//false
+         $user->isClean('username');//fa;se
+         $user->isClean('nama');//true
+         $user->isClean('nama', 'username');//true
+
+         $user->save();
+
+         $user->isDirty();//false
+         $user->isClean();//true
+         dd($user->isDirty());
     }
 }
