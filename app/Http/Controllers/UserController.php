f95b1ec6a5645b161c7619381cb622bc4732a6d2
deanissa <deanissa604@gmail.com>
2025-03-12 11:01:07 +0700

    wasChanged

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,28 +13,21 @@ class UserController extends Controller
 
         //coba akses model UserModel
         $user = UserModel::create(
-        ['username' => 'manager55', 
-         'nama' => 'Manager55',
+        ['username' => 'manager11', 
+         'nama' => 'Manager11',
          'password' => Hash::make('12345'), // Password tetap dimasukkan
          'level_id' => 2
         ]
         );
-         $user->username = 'manager55';
-
-         $user->isDirty();//true
-         $user->isDirty('username');//true
-         $user->isDirty('nama');//false
-         $user->isDirty('nama', 'username');//false
-
-         $user->isClean();//false
-         $user->isClean('username');//fa;se
-         $user->isClean('nama');//true
-         $user->isClean('nama', 'username');//true
+         $user->username = 'manager12';
 
          $user->save();
 
-         $user->isDirty();//false
-         $user->isClean();//true
-         dd($user->isDirty());
+         $user->wasChanged();//true
+         $user->wasChanged('username');//true
+         $user->wasChanged('username', 'level_id');//true
+         $user->wasChanged('nama');//false
+
+         dd($user->wasChanged(['nama', 'username']));//true
     }
 }
