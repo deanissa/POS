5b903cf2e901870b1e74eacac64e11214124d253
deanissa <deanissa604@gmail.com>
2025-04-11 23:37:29 +0700

    jobsheet 5 - praktikum 1 - @yield ('content')

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -37,35 +37,10 @@
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    @include('layouts.breakcrumb')
+    @include('layouts.breadcrumb')
     <!-- Main content -->
     <section class="content">
-
-      <!-- Default box -->
-      <div class="card">
-        <div class="card-header">
-          <h3 class="card-title">Title</h3>
-
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
-          </div>
-        </div>
-        <div class="card-body">
-          Start creating your amazing application!
-        </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-          Footer
-        </div>
-        <!-- /.card-footer-->
-      </div>
-      <!-- /.card -->
-
+    @yield('content')
     </section>
     <!-- /.content -->
   </div>
