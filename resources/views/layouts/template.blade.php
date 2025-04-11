d5a15d10cc26d5e03c6cd9915d6437150a5a3614
deanissa <deanissa604@gmail.com>
2025-04-11 23:25:06 +0700

    jobsheet 5 - praktikum 1 - membuat file footer.blade.php

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -31,7 +31,7 @@
     <!--sidebar-->
     @include('layouts.sidebar')
     <!--/.sidebar-->
-    
+
   </aside>
 
   <!-- Content Wrapper. Contains page content -->
@@ -86,15 +86,8 @@
   </div>
   <!-- /.content-wrapper -->
 
-  <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
-    </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
+  <!--footer-->
+  @include('layouts/footer')
     <!-- Control sidebar content goes here -->
   </aside>
   <!-- /.control-sidebar -->
